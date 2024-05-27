106ed87b364419ae12482a4537d5b74efcd72d76
MaLikTTCCCC <maliktrc@outlook.com>
2024-05-27 09:14:38 +0200

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,14 @@ import yfinance as yf
 #import matplotlib as mpl
 #import matplotlib.dates as mdates
 #from datetime import datetime
+import altair as alt
+import vega_datasets as vd
 
 
 msft = yf.download("msft", start="2020-01-01", end="2024-01-01")
 dates=msft.index
 
+chart=alt.Chart(msft)
+c=chart.markpoint()
 
-st.line_chart(msft['Close'],title='Close MSFT')
+st.altair_chart(c)
